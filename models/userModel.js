66c4ad30de569687608991321cc90935e6175e69
enduranceighobue <ighobuendy@email.com>
2026-01-18 13:51:56 +0100

    updates

--- models/userModel.js
+++ models/userModel.js
@@ -53,13 +53,13 @@ const userSchema = new Schema(
 );
 
 // hashing password
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
+
 
 
 
